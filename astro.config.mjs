8d005c6b739e2970bdbaa8447c205ff009dacb7c
samson <samson.nigusse7@gmail.com>
2026-08-25 13:27:55 -0500

    Fix GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://afrocoder16.github.io',
+  base: '/Aruma-Tacos-',
   integrations: [tailwind({ applyBaseStyles: false })],
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
