4c35289601e14486201d0347992d386c1cf8364c
samson <samson.nigusse7@gmail.com>
2026-09-10 09:03:47 -0500

    Deploy static site to Hostinger

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://afrocoder16.github.io',
-  base: '/Aruma-Tacos-',
+  site: 'https://arumatacosmn.com',
+  output: 'static',
   integrations: [tailwind({ applyBaseStyles: false })],
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
